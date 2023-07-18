9ef533b1e70b638fca99e386714f34b10369ddef
Shanib c k <ckshanib8@gmail.com>
2023-07-18 11:08:12 +0530

    background and text color added

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+int _counter = 0;
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _counter.toString(),
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white38),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          //Incrementing Data
+          print(_counter);
+          _counter = _counter + 1;
+        },
         child: Icon(
           Icons.add,
         ),
