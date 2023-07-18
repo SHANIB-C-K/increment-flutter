42051449fb2a9106b033afb4e901ba6fce0dc99e
Shanib c k <ckshanib8@gmail.com>
2023-07-18 10:40:53 +0530

    mainApp created

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
