4d7890eb7dff53ea107cdd29fe647505da1ce93f
Shanib c k <ckshanib8@gmail.com>
2023-07-18 16:15:40 +0530

    increment button color changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
       home: HomeScreen(),
     );
   }
